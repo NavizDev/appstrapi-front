a1e1c7ff2f52d5c076b7a606cdd230d443a01673
Juan <juansilupumaza@gmail.com>
2020-04-03 22:27:54 -0500

    hot fix purgecss

--- postcss.config.js
+++ postcss.config.js
@@ -1,15 +1,13 @@
-const purgecss = [
-  "@fullhuman/postcss-purgecss",
-  {
-    content: ["./components/**/*.js", "./pages/**/*.js"],
-    defaultExtractor: (content) => content.match(/[\w-/:]+(?<!:)/g) || [],
-  },
-];
+const purgecss = require("@fullhuman/postcss-purgecss")({
+  // Specify the paths to all of the template files in your project
+  content: ["./pages/**/*.js", "./components/**/*.js"],
 
+  // make sure css reset isnt removed on html and body
+  whitelist: ["html", "body"],
+
+  // Include any special characters you're using in this regular expression
+  defaultExtractor: (content) => content.match(/[\w-/:]+(?<!:)/g) || [],
+});
 module.exports = {
-  plugins: [
-    require("tailwindcss"),
-    require("autoprefixer"),
-    process.env.NODE_ENV === "production" ? [purgecss] : [],
-  ],
+  plugins: [require("tailwindcss"), require("autoprefixer"), purgecss],
 };
